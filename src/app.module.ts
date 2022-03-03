8d86a230ab3526555c50462770b64df8b05b345b
Artem Tarasenko <shabashab.04@gmail.com>
2022-03-03 17:02:02 +0200

    Initialize authentication module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { AuthenticationModule } from "./authentication/authentication.module";
 
 @Module({
-  imports: [],
+  imports: [AuthenticationModule],
   controllers: [],
   providers: [],
 })
