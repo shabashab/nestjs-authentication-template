d3f97180e7a8fb7cc7719351563fb6bcf798a9bf
Artem Tarasenko <shabashab.04@gmail.com>
2022-03-03 17:16:40 +0200

    Add typeorm dependency and create user entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+	@PrimaryGeneratedColumn("uuid")
+	public id: string;
+
+	@Column({
+		unique: true
+	})
+	public username: string;
+
+	public passwordHash: string;
+}
